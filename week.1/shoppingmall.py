73c3c5126bff6722b738cbab37552966c289fc0c
Zachary Derrick <derrick.zachary@gmail.com>
2025-04-13 20:14:42 +0000

    edited names and map

--- week.1/shoppingmall.py
+++ week.1/shoppingmall.py
@@ -9,14 +9,19 @@
 print("Weyland-Yutani Spaceport Mall".center(58))
 print("★·.·★·.·★·.·★·.·★·.·✦·.·★·.·★·.·★·.·★·.·★·.·✦·.·★·.·★·.·★")
 print("Helping You Build a Better World!".center(58))
-print("\n" * 2 + "STATION DEPARTMENTS")
-print("""Spacecraft:\n   -Correllian Engineering: Bay-A1   
+print("\n" * 2 + "STATION DEPARTMENTS".center(58))
+print("""\nSpacecraft:\n   -Correllian Engineering: Bay-A1   
    -Wallace Co.: Bay A-2 \n   -Stroud Eklund: Bay-C2""")
 print("""Nanotechnology:\n   -Krei Tech: Bay-B2\n   -Tyrell Corp: Bay-C2
    -Cyberdyne: Bay-C1""")
 print("""FTL Travel:\n   -Stargate: Bay-B2\n   -Olympic Carriers: Bay-C1
    -Aniara: Bay-C2""")
-
+print("""Holographics:\n   -Foundation: Bay-A1\n   -Holodeck: Bay-B1
+   -Gabor-Holo: Bay-C1""")
+print("""Robotics:\n   -Industrial Automation: Bay-R2\n   -Cyberdyne Systems: Bay-C1
+   -Delos Corp: Bay-C2""")
+print("""\n\033[1m✦ In case of an emergency, all bays will go into immediate lockdown
+and follow Special Order 937 in the Weyland-Yutani handbook.✦\033[0m """)
 # print(r""") this r is used for 'raw strings' allowing the text to be printed and 
 # not interpreted as escape characters. It prints the string literally.
 print(r"""
@@ -27,7 +32,7 @@ print(r"""
      / /  [BAY-A1] [BAY-A2] \ \
     | |   [BAY-B1] [BAY-B2]  | |
     | |         CORE         | |
-     \ \  [BAY-C1] [BAY-C2]  / /
-      '.'.     VENTRAL     .'.' 
-        '-.'-.__________.-'-'
+     \ \  [BAY-C1] [BAY-C2] / /
+      '.'.     VENTRAL    .'.' 
+        ' -.___________.' '
 """)
