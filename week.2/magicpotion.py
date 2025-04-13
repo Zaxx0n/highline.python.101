bad4bdb1d4ec6cfc02683f156a8ccba7cda5c8ea
Zachary Derrick <derrick.zachary@gmail.com>
2025-04-13 21:15:24 +0000

    updated the sizes and how they are formulated

--- week.2/magicpotion.py
+++ week.2/magicpotion.py
@@ -2,31 +2,33 @@
 # Assignment: Week 2 - Magic Potion Recipe
 # Author: Zachary Derrick                    
 # Date of latest revision:  4/13/2025   
-# Purpose: write a Python program that takes in user input for
-#          a few data, calculates the needed amount of each ingredient,
+# Purpose: Write a Python program that takes in user input for
+#          a few pieces of data, calculates the needed amount of each ingredient,
 #          and outputs a magic potion recipe.
 # Resources: A02A - Magic Potion Recipe Word Document, ChatGPT, 
 #            Variables and Print Sample: https://youtu.be/03MGu250mqU?si=HHj-IILJ-xc-nsse
+# Notes: I decided to go with a menu that allows the user to select from 
+#        a list of cauldrons. An advanced version of this would allow the user to
+#        input their own cauldron name and its size as an additional option.
 
 # libraries
 import math
 
-# I decided to go with a menu that allows the user to select from 
-# a list of "pots". An advanced version of this would allow the user to
-# input their own cauldron and its size.
+# main
 cauldrons = [
-    {"cauldronName": "Pixie's Pan " + "🧚", "cleanName": "Pixie's Pan", "size": .25},      
-    {"cauldronName": "Goblin's Goblet 🧌 ", "cleanName": "Goblin's Goblet", "size": 1},  
+    {"cauldronName": "Pixie Pan " + "🧚", "cleanName": "Pixie Pan", "size": .50},      
+    {"cauldronName": "Goblin Goblet 🧌 ", "cleanName": "Goblin Goblet", "size": 1},  
     {"cauldronName": "Witchfire Basin 🔥", "cleanName": "Witchfire Basin", "size": 2.5},
-    {"cauldronName": "Elder Warlock's Vat 🔮", "cleanName": "Elder Warlock's Vat", "size": 5},  
-    {"cauldronName": "Dragon's Tea Kettle 🐉", "cleanName": "Dragon's Tea Kettle", "size": 10}    
+    {"cauldronName": "Elder Warlock Vat 🔮", "cleanName": "Elder Warlock Vat", "size": 5},  
+    {"cauldronName": "Dragon Tea Kettle 🐉", "cleanName": "Dragon Tea Kettle", "size": 10}    
 ]
 
 # gives the user a list of common caudrons to choose from and includes
 # its size in liters
-print("Common Cauldrons")
+print("Common Cauldrons:")
+# enumerate takes a list and returns pairs of index and value, here cauldruns and its number in the list
 for i, cauldron in enumerate(cauldrons, 1):
-    liters = cauldron["size"] * 5
+    liters = cauldron["size"] 
     print(f"{i}) {cauldron['cauldronName']} — {liters} Liters")
 
 # user selection
@@ -37,20 +39,24 @@ while True:
             selectedCauldron = cauldrons[choice - 1]
             break
         else:
-            print("That is not a known option.")
+            print("That is not an option.")
     except ValueError:
-        print("Please enter a number, Pleb.")
+        print("Please enter a number from the list. (1-5)")
 
-potSizeLiters = selectedCauldron["size"] * 5
+potionLiters =  selectedCauldron["size"]
 
 # get number of plants
 plants = int(input("How many plants will you be growing?: "))
+# we need to check if the plants are an even number for later so this makes sure the number 
+# of plants is divisible by 2 with no remainder
+evenPlants = plants if plants % 2 == 0 else plants + 1
 
 # calculations
-crystalWater = 0.5 * potSizeLiters
+crystalWater = 0.5 * potionLiters
+# roundedWater = math.ceil(crystalWater * 100) / 100  - I was playing around with rounding to the nearest hundredth 
 moonDust = 10 * plants
-# math.ceil rounds the result to the nearest whole number
-sunflowerPetals = math.ceil((3 * plants) / 2)
+sunflowerPetals = (evenPlants // 2) * 3
+# math.ceil rounds up the result to the nearest whole number since we area using eyes as a counter
 eyeofNewt = math.ceil(sunflowerPetals / 5)
 # print the recipe to the screen. 
 print(f"\n🧪 Ingredients Needed to Grow {plants} Plants Using Your {selectedCauldron['cleanName']}: ")
