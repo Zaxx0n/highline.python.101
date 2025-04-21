18a2359808d3da395d08d22edc3aa34241a7a72b
Zachary Derrick <derrick.zachary@gmail.com>
2025-04-21 00:24:17 +0000

    version submitted

--- week.3/grocerybudgetcalc.py
+++ week.3/grocerybudgetcalc.py
@@ -6,11 +6,14 @@
 #         by creating a real-world application - a grocery budget calculator.
 # Resources: A02B - Grocery Budget Calculator
 #            
-# Notes: 
+# Notes: Because there is so much repetition in what the code is supposed to do, I decided to make a loop and a function.
+#        There is a version of this that I was working on that uses the os library and loads a log so that previous months
+#        could be imported or compared.  I eventually just simplified it to the scritp below.
 
 # libraries
 import math
 
+# functions
 def get_item_info(item_num):
     item = input(f"Item {item_num} name: ")
     print(item)
@@ -19,12 +22,15 @@ def get_item_info(item_num):
     total = qty * price
     return item, total
 
+# main
+# below the user inputs their budget and is presented with information about the following steps
 monthly_budget = float(input("How much is your monthly budget: $"))
-
 print("Next we need to gather some information. Please enter three items you've recently purchased:")
-
+# the loop uses the following empty list to append to and starts the total expenditure as variable 0
 item_totals = []
 total_expenditure = 0
+# for loop: I used a for loop because of the repition in my original code this loop uses the fuction get_item_info
+# three times, or a range of (1, 4)
 for i in range(1, 4):
     item_name, item_total = get_item_info(i)
     item_totals.append((item_name, item_total))
@@ -37,9 +43,5 @@ for name, total in item_totals:
 
 remaining_budget = float(monthly_budget - total_expenditure)
 
-rounded_expenditure = math.ceil(total_expenditure)
 print(f"\nYour total expenditure is: ${total_expenditure:.2f}")
 print(f"Your remaining budget is: ${remaining_budget:.2f}")
-
-# rounded_expenditure = math.ceil(total_expenditure)
-# print(f"Your total expenditure is: ${rounded_expenditure}")
